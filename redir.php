9fb0443ce90e1506ec13679be57d5f909a203d9f
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-08 00:46:39 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,1 +1,1 @@
-<?php header('location: https://np.lexity.com/f57ea75d/h/1/v/q/k/aaa/u/xxxx/r/fff/vn/1/c/<script>alert(0);</script>//') ?> 
+<?php header('location: https://np.lexity.com/f57ea75d/h/1/v/q/k/aaa/u/xxxx/r/fff/vn/1/c/<script>alert(0);<%2fscript>//') ?> 
