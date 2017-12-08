5401baad4bca5f06776c4d98c9a5e86400d4f988
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-08 01:57:00 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,1 +1,1 @@
-<?php header('location: http://httpbin.org/response-headers?Content-Type=text%2Fplain%3B+charset%3DUTF-8&Server=%3Cscript%3Ealert(0)%3C/script%3E/') ?> 
+<?php header('location: https://httpbin.org/response-headers?Content-Type=text%2Fplain%3B+charset%3DUTF-8&Server=%3Cscript%3Ealert(0)%3C/script%3E/') ?> 
