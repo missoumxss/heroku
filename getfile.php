ea440214ba70dc11ae127b3cb0e11e2c228ae8f7
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-09 03:49:06 +0100

    Update getfile.php

--- getfile.php
+++ getfile.php
@@ -1,5 +1,19 @@
-<?php 
+<?php
+$host = "https://explorer.earthengine.google.com/";
+$path = "/getProject";
+$data = "state=aaaaxxxx";
+$data = urlencode($data);
 
-echo file_get_contents("https://explorer.earthengine.google.com/getProject");
-
+header("POST $path HTTP/1.1\\r\
+" );
+header("Host: $host\\r\
+" );
+header("Content-type: application/x-www-form-urlencoded\\r\
+" );
+header("Content-length: " . strlen($data) . "\\r\
+" );
+header("Connection: close\\r\
+\\r\
+" );
+header($data);
 ?>
