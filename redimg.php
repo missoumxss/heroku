ecfe3418086a71c28f62be3fa2203ada52b32d62
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-11 21:15:05 +0100

    Update redimg.php

--- redimg.php
+++ redimg.php
@@ -1,2 +1,2 @@
-<?php header('location: https://www.google.com/analytics/partners/owner/img/company/5112883335659520/assets/5668600916475904')?>
+<?php header('location: https://spreadsheets.google.com/feeds/list/1Bfzn9YcSmleNhu4cat0VPGgNE3C4PWwB9vFxF5BjXfw/od6/public/values/cre1l')?>
 
