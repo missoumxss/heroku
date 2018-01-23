c3d9948e8a9b586fb9ed049ab79b079beb804fca
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-23 23:09:40 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,3 +1,3 @@
 <?php
-header('location: https://archive.uber.com/pypi/packages/source/I/IgnoreMe_mime/IgnoreMe_mime-1.0.1.tar.gz');
+header('location: https://bughunt1307.herokuapp.com/xsssnif.php');
 ?>
