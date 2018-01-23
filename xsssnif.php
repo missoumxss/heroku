20b5186b22aa7c1d932c4f9927775fbc61520277
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-23 23:16:31 +0100

    Update xsssnif.php

--- xsssnif.php
+++ xsssnif.php
@@ -3,5 +3,5 @@ header('Content-Type: application/octet-stream');
 header('X-Content-Type-Options: nosniff');
 ?>
 
-<pre></pre><html><script>alert(0)</script></html><pre></pre>
+<html><script>alert(0)</script></html>
 
