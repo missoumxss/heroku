43c005b0fc610fb36507da077fbbab9f07e38ef7
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-23 23:48:58 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,3 +1,4 @@
 <?php
-header('location: https://r4---sn-hgn7rn7k.c.drive.google.com/videoplayback?id=9600d2fc168f1372&itag=18&source=webdrive&requiressl=yes&mm=30&mn=sn-hgn7rn7k&ms=nxu&mv=m&pl=16&sc=yes&ttl=transient&ei=JblnWq7BHoPQ-QWVxKGIAQ&susc=dr&driveid=1MXNlo0Ws71NCdqxzvaVLG2-kgZBYyJPq&app=explorer&mime=video/mp4&lmt=1516742095435030&mt=1516746988&ip=41.102.240.152&ipbits=0&expire=1516750645&cp=QVNGWEhfT1ZSRlhOOkdGRHhKY1RuSlll&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,sc,ttl,ei,susc,driveid,app,mime,lmt,cp&signature=854076158B312AC83DC0F725519D4EB06F530474.42DCFB1735D8A4667D4029C174B6E5EADD12AA81&key=ck2&cpn=oYxmlHs50mEKgGzY&c=WEB_EMBEDDED_PLAYER&cver=20180122');
+header('location: https://hackone.slack.com/api/conversations.history?_x_id=778c508a-1516744884.811&channel=C8B29PKL7&limit=42&ignore_replies=true&include_pin_count=true&inclusive=true&no_user_profile=true&latest=1512491510.000125%3Ca%20href=x%3Exsqq&token=xoxs-272293041936-272872958131-272293052992-6019a3cfd8&visible=1');
+
 ?>
