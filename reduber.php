cec8f3f1032879565939200d4bdc3499e2625e3f
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-09 02:05:05 +0100

    Update reduber.php

--- reduber.php
+++ reduber.php
@@ -1,1 +1,5 @@
-<?php header('location: https://archive.uber.com/pypi/packages/source/I/IgnoreMe_mime/IgnoreMe_mime-1.0.1.tar.gz')?>
+<?php
+
+header('location: https://archive.uber.com/pypi/packages/source/I/IgnoreMe_mime/IgnoreMe_mime-1.0.1.tar.gz');
+header('Content-Disposition: inline');
+?>
