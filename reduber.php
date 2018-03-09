48ab698ef98e930002873b48306265a1c2ef89d7
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-09 02:06:33 +0100

    Update reduber.php

--- reduber.php
+++ reduber.php
@@ -1,5 +1,8 @@
 <?php
 
-header('location: https://archive.uber.com/pypi/packages/source/I/IgnoreMe_mime/IgnoreMe_mime-1.0.1.tar.gz');
 header('Content-Disposition: inline');
+
 ?>
+
+
+<iframe src="https://bughunt1307.herokuapp.com/reduber.php">
