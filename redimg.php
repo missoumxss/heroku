6cca082d93ebc84a0e06f87eccd3069f4a118fdb
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-13 20:35:18 +0100

    Update redimg.php

--- redimg.php
+++ redimg.php
@@ -1,2 +1,2 @@
-<?php header('location: http://us.api.semrush.com/?action=report%23type=domain_rank%23domain=i<img%2Fsrc=x>aa.wordpress.com')?>
+<?php header('location: http://us.api.semrush.com/?action=report%23type=domain_rank%23domain=i<img/src=x>aa.wordpress.com')?>
 
