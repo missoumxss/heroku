bfc6b87bed4c0f5471f9cb27380a1cf962770290
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-14 02:12:59 +0100

    Update fbmessenegerxss.php

--- fbmessenegerxss.php
+++ fbmessenegerxss.php
@@ -3,5 +3,5 @@ header('Content-type: application/octet-stream');
 header('X-content-type-options: nosniff');
 ?>
 <html><script>alert('it should be downloadable instead ')</script>
-this page's content type is application/octet-stream and has X-content-type-options: nosniff implemented, hencethis content shoulnt be rendered!!
+this page's content type is application/octet-stream and has X-content-type-options: nosniff implemented, hence this content shouln't be rendered!!
 </html>
