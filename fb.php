61d84ff224a7d9adae5487c2379f71e5126cb992
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-17 01:57:37 +0100

    Update fb.php

--- fb.php
+++ fb.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/octet-stream');
-header('X-content-type-options	nosniff');
+header('X-content-type-options: nosniff');
 
 ?>
 <html><script>alert(0)</script></html>
