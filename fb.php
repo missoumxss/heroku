1b98ec82733960e6fcaaf13c2fbadc1b58f81543
missoum1307 <missoum1307@users.noreply.github.com>
2018-04-02 22:15:18 +0100

    Update fb.php

--- fb.php
+++ fb.php
@@ -1,3 +1,3 @@
 <?php 
-header('Location: https://www.google.com/safebrowsing/alerts/javascript/astools.js;/xx.%2e/.%2e')
+header('Location: https://www.google.com/safebrowsing/alerts/javascript%2Fastools.js;%2F..%2F..%2F..%2F..%2F/xxxxxx')
 ?>
