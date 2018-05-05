60ecaa6032cc0258e36edfc5a794279e80ff31cd
missoum1307 <missoum1307@users.noreply.github.com>
2018-05-05 19:27:40 +0100

    Update log.php

--- log.php
+++ log.php
@@ -1,13 +1,11 @@
 <?php
-// Getting the information 
+
 $ipaddress = $_SERVER['REMOTE_ADDR']; 
-$page = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";  
-$page .= iif(!empty($_SERVER['QUERY_STRING']), "?{$_SERVER['QUERY_STRING']}", ""); 
 $referrer = $_SERVER['HTTP_REFERER']; 
 $datetime = mktime(); 
 $useragent = $_SERVER['HTTP_USER_AGENT']; 
 $remotehost = @getHostByAddr($ipaddress);
 
 
-file_put_contents("log.txt",print_r($ipaddress . '|' . $referrer . '|' . $datetime . '|' . $useragent . '|' . $remotehost . '|' . $page . "\n"; ,true));
+file_put_contents('log.txt',print_r($ipaddress . '|' . $referrer . '|' . $datetime . '|' . $useragent . '|' . $remotehost . '\n'; ,true));
 ?> 
