cf99047418e6185644664040a1a1248070f3a177
missoum1307 <missoum1307@users.noreply.github.com>
2018-06-02 04:20:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include 'log.php';
 <!doctype html>
 <html amp lang="en">
   <head>
+    <base href=x>
     <meta name="google-site-verification" content="nCeEmY1DVP_k55IELPFaI8oPiW7KVqqp_SuuUjC2yT8" />
 <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 
@@ -42,11 +43,13 @@ include 'log.php';
   allowfullscreen
   frameborder="0"
   src="https://mail.google.com/mail/u/2/">
-     <amp-img layout="fill" src="https://foo.com/foo.png" placeholder></amp-img>
+     <amp-img layout="fill" src="https://foo.com/foo.png" onmouseover="alert(1)" placeholder></amp-img>
 </amp-iframe>
 
-<textarea>xxxe.com/"><base href="javascript:\"><a href="/xss//alert(document.domain)"></textarea>
+<textarea onmousover="alert(1)">xxxe.com/"><base href="javascript:\"><a href="/xss//alert(document.domain)"></textarea>
 
-
+<embed src=x.swf>
+  <object data=x.swf>
+    
   </body>
 </html>
