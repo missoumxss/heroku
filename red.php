4339fd9df78339e395ece2aacd46c16959854de3
missoum1307 <missoum1307@users.noreply.github.com>
2018-06-16 02:57:44 +0100

    Update red.php

--- red.php
+++ red.php
@@ -1,1 +1,1 @@
-<?php header('Location: https://www.google.com/safebrowsing/alerts/javascript/astools.js;/%2e%2e/%2e%2e/1337'); ?> 
+<?php header('Location: https://www.google.com/safebrowsing/alerts/javascript/astools.js;/foo/bar/%2e%2e/baz'); ?> 
