a32ff5f60e28f4bcce64a7b7de887a52a6f3ed9b
missoum1307 <missoum1307@users.noreply.github.com>
2018-06-23 02:29:19 +0100

    Update red.php

--- red.php
+++ red.php
@@ -1,1 +1,1 @@
-<?php header('Location: https://www.google.com/safebrowsing/alerts/javascript/astools.js;/%25#x2e;%25#x2e;/%252e%252e/aaa'); ?> 
+<?php header('Location: http://bughunt1307.herokuapp.com/redirector.php?x=https://www.google.com/safebrowsing/alerts/javascript/astools.js;/%252e./%252e./%252e./%252e./evil'); ?> 
