13e811456760de3794f67d6b5fc3655ae03dade6
missoum1307 <missoum1307@users.noreply.github.com>
2018-12-17 23:15:52 +0100

    Update xss.php

--- xss.php
+++ xss.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-Type: text/csv; charset=utf-8");
-
+header("X-Content-Type-Options: nosniff");
 header("Content-Disposition: attachment;filename="aaa.csv";filename*=UTF-8''aaa.csv");
 
 
