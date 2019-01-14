86e0ac23420a893b153a8c5ff02f1cbe9ce03e31
missoum1307 <missoum1307@users.noreply.github.com>
2019-01-14 15:17:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,12 @@ include 'log.php';
 <!doctype html>
 <html amp lang="en">
   <head>
+    <meta name="amp-experiments-opt-in" content="amp-next-page">
     <meta name="google-site-verification" content="nCeEmY1DVP_k55IELPFaI8oPiW7KVqqp_SuuUjC2yT8" />
 <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 
 </amp-script>
+  <meta name="amp-experiments-opt-in" content="amp-next-page">
     <meta name="google-site-verification" content="r3gW0wqZv7JHFJLQlZ_W_kd7WvVYyoHYc7PZz_Aul1g" />
     <meta charset="utf-8">
     <script async src="https://cdn.ampproject.org/v0.js"></script>
