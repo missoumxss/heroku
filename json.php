16c9b7ddf55caffbc32825127744d33f899382a5
missoum1307 <missoum1307@users.noreply.github.com>
2019-11-24 16:41:11 +0100

    Update json.php

--- json.php
+++ json.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 ?>
 {
-  "audio": "xxxxxxxx",
+  "audio": "https://cs1.mp3.pm/listen/160751836/S1FZQmpiOVNLdFo1cEtTNlNGc0VjNEtKK1o4dms5VmhabS9vL3RFb2tBRTR0Q3dvQjZPaStybWk3SnJaMFl0RmpCWW1FUytRR1F4cmM3eEpuQ2FWNFVCWlZ4bVZNWUVrS1BDRmFBV3RmVnBoRVlrcG9TUEs4eWlwQzFmaXNqM3c/Aramis_-_21_Girls_(mp3.pm).mp3",
   "image": "",
   "isStereo": true,
   "isYawOnly": false,
