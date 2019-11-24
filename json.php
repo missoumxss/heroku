b8138bb1aaa89e34ef4205e7df7edc4c562e6f83
missoum1307 <missoum1307@users.noreply.github.com>
2019-11-24 15:57:27 +0100

    Update json.php

--- json.php
+++ json.php
@@ -4,10 +4,10 @@ header('Access-Control-Allow-Origin: *');
 ?>
 {
   "audio": "xxxxxxxx",
-  "image": "https://bughunt1307.herokuapp.com/ssrf1.svg",
+  "image": "https://bughunt1307.herokuapp.com/svg/ssrf1.svg",
   "isStereo": true,
   "isYawOnly": false,
-  "preview": "https://bughunt1307.herokuapp.com/ssrf1.svg",
+  "preview": "https://bughunt1307.herokuapp.com/svg/ssrf1.svg",
   "video": "",
   "pitch": -0.62,
   "yaw": 1.6
