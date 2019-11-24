55f3177c663e4c90ec54ec7b320b0e91085e2f2d
missoum1307 <missoum1307@users.noreply.github.com>
2019-11-24 07:42:49 +0100

    Update json.php

--- json.php
+++ json.php
@@ -7,7 +7,7 @@ header('Access-Control-Allow-Origin: *');
   "image": "http://bef4c411.ngrok.io/evil.jpg",
   "isStereo": true,
   "isYawOnly": false,
-  "preview": "<a href=x>xss",
+  "preview": "%3Ca%20href=x%3Exss",
   "video": "",
   "pitch": -0.62,
   "yaw": 1.6
