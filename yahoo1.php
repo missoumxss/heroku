cd57963681c51dc0d43ed1d38f422212c744f821
missoum1307 <missoum1307@users.noreply.github.com>
2020-03-02 21:08:28 +0100

    Update yahoo1.php

--- yahoo1.php
+++ yahoo1.php
@@ -1,6 +1,6 @@
 <?php 
 
-header("Location: https://tourbuilder.withgoogle.com/tours/ahJzfmd3ZWItdG91cmJ1aWxkZXJyEQsSBFRvdXIYgICg7tb6oAsM/csv");
+header("Location: https://r1---sn-h5q7kne6.c.youtube.com/videoplayback?expire=1583188977&ei=kTddXsL1JpHl1gavhbZw&ip=37.134.155.114&cp=U0pXSFFTVl9MUUNQN19JTVpKOm45SkwwWE9ITkw1WjZKWlpVWnBBM3dySmY2c3Y5U0NNVXFLb2YzZXRNOGw&id=o-ABclPTCzn1Kxs1GcCUyGuZr3mjPn6cde5bD48FLqZLem&itag=251&source=youtube&requiressl=yes&mm=30%2C26&mn=sn-h5q7kne6%2Csn-25glen7r&ms=nxu%2Conr&mv=m&mvi=0&pl=22&sc=yes&initcwndbps=1746250&vprv=1&prv=1&mime=audio%2Fwebm&gir=yes&clen=753&otfp=1&dur=1.021&lmt=1583167280836734&mt=1583167283&fvip=1&keepalive=yes&fexp=23842630&c=WEB&txp=6211222&sparams=expire%2Cei%2Cip%2Ccp%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cprv%2Cmime%2Cgir%2Cclen%2Cotfp%2Cdur%2Clmt&sig=ADKhkGMwRQIhAM62gJPtl8Xt9-UclPY9E5GzSZFdPeBAfzy6-1T9hPWRAiBFPz7PXC_lwym7JLrGwFXklAI4agX6WHrx4Am4rEP5lw%3D%3D&lsparams=mm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Csc%2Cinitcwndbps&lsig=ABSNjpQwRAIgeX5Tj7NLCuiL2qmdW1m9dpn7pvw07ipZ9fwfudr6w6sCIBXlnFXabMxZ1607L_43f_bDXAyNucRx6Tma6ZB90unJ&alr=yes&cpn=LTejbW2WhDZFssb9&cver=2.20200229.10.00&range=0-276&rn=2&rbuf=0");
 
 
 
