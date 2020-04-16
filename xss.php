277e9d40c5649c0f6bf40f7613d5971944c7e053
missoum1307 <missoum1307@users.noreply.github.com>
2020-04-16 17:26:50 +0200

    Update xss.php

--- xss.php
+++ xss.php
@@ -1,9 +1,3 @@
 <?php
-
-header("Content-Type: text/csv; charset=utf-8");
-header("X-Content-Type-Options: nosniff");
-header("Content-Disposition: attachment;filename="aaa.csv";filename*=UTF-8''aaa.csv");
-
-
-echo "Hello: ".$_GET["name"];
+echo "Hello: ".$_GET["input"];
 ?>
