a1459d680bcf06df945a42595aae308d832fe71f
missoum1307 <missoum1307@users.noreply.github.com>
2020-05-12 22:20:51 +0200

    Update link-header.php

--- link-header.php
+++ link-header.php
@@ -1,4 +1,6 @@
 <?php
-header('Link: <https://0c234d97.ngrok.io/styles.css>; rel=preload; as=style');
+// header('Link: <https://0c234d97.ngrok.io/styles.css>; rel=preload; as=style');
 header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/javascript');
 ?>
+alert(location)
