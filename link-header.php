3329267dba8afb30713ce0cf14a4d7bc7257bf16
missoum1307 <missoum1307@users.noreply.github.com>
2020-05-13 01:28:31 +0200

    Update link-header.php

--- link-header.php
+++ link-header.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Link: <javasript:alert(1)>; rel=preload; as=script,?key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM>; rel=preload');
+header('Link: <javasript:alert(1);//>; rel=preload; as=script,?key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM>; rel=preload');
 header('Content-Type: text/html');
 ?>
 <!DOCTYPE html>
